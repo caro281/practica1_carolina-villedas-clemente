5f60a9fbbb86ee1558168c64c14d5b7cf4342843
caro281 <89856829+caro281@users.noreply.github.com>
2021-09-06 17:33:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-
+function getIMC(){
+  var peso=parsefloat(document.getElementById('peso ').value);
+  var altura=parsefloat(documet.getElementById('altura').value);
+  var imc=peso/(altura*altura);
+  documentgetElementById("imc")innerHTML=imc;
